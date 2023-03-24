952ff8416144535fdb886dcfe928f29938943c54
loramisautova <misautova.lora@gmail.com>
2023-03-24 19:54:14 +0300

    Add main page

---
+++ src/components/TodoList/index.tsx
@@ -0,0 +1,84 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { List } from 'antd';
+
+import { TodoItem } from '../../types';
+import { TodayItem } from '../TodayItem';
+
+// import styles from './styles.module.scss';
+
+type Props = {
+    todos: TodoItem[] | undefined;
+    // onEditTask?: (id: string, newTask: string) => void;
+};
+
+export const TodayList: React.FC<Props> = ({ todos}) => {
+    const containerRef = useRef(null);
+    const firstItemRef = useRef(null);
+    // стейт - id редактированного элемента
+    const [editItemId, setEditItemId] = useState<string>();
+
+    useEffect(() => {
+        let observer: IntersectionObserver;
+        const container = containerRef.current;
+        const firstElement = firstItemRef.current;
+
+        console.log('##############');
+        console.log('container', container);
+        console.log('firstElement', firstElement);
+        console.log('##############');
+
+        if (container) {
+            observer = new IntersectionObserver((entries) => {
+                const [ entry ] = entries;
+
+                console.log('##############');
+                console.log('entries', entries);
+                console.log('entry.isIntersecting', entry.intersectionRatio);
+                console.log('##############');
+
+            }, {
+                root: container,
+                rootMargin: "0px",
+                threshold: [1],
+            });
+
+            if (firstElement) {
+                observer.observe(firstElement);
+            }
+        }
+
+        return () => {
+            if(observer && firstElement) {
+                observer.unobserve(firstElement);
+            }
+        }
+    }, [containerRef, firstItemRef])
+
+    const handleItemEditToggle = (id: string) => {
+        if (editItemId === id) {
+            setEditItemId('');
+        } else {
+            setEditItemId(id);
+        }
+    }
+
+    return Boolean(todos?.length)
+        ? (
+            <div ref={containerRef}>
+                <List
+                    dataSource={todos}
+                    renderItem={(todo, index) => (
+                        <div ref={index === 0 ? firstItemRef : undefined}>
+                            <TodayItem
+                                key={todo.id}
+                                item={todo}
+                                isEditMode={editItemId === todo.id}
+                                onEditToggle={handleItemEditToggle}
+                            />
+                        </div>
+                    )}
+                />
+            </div>
+        )
+        : null;
+}
